db176c162af63913bc47faf4e5091e9f6b7e7e71
DANIEL CABRAL DA SILVA <cabralcwb2019@gmail.com>
2025-09-10 17:22:48 -0300

    feat:O try-catch foi trocado por Java Streams, e o GlobalExceptionHandler agora retorna erros de valor inválido para Status de Reserva ao front-end, em vez de lançar uma RuntimeException.

---
+++ src/main/java/br/com/senior/gestaohospedes/belavista/exception/EnumNaoEncontradoException.java
@@ -0,0 +1,12 @@
+package br.com.senior.gestaohospedes.belavista.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EnumNaoEncontradoException extends BusinessException {
+
+  public EnumNaoEncontradoException(String valor) {
+    super("error.enum.naoEncontrado", valor);
+  }
+}
